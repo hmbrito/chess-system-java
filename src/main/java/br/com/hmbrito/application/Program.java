3a81342cf9f09c9c0986df2cabf3a379c05523c8
hmbrito <hamiltonmbrito@yahoo.com>
2021-04-06 17:50:55 -0300

    First class: Position

--- src/main/java/br/com/hmbrito/application/Program.java
+++ src/main/java/br/com/hmbrito/application/Program.java
@@ -1,9 +1,14 @@
 package br.com.hmbrito.application;
 
+import br.com.hmbrito.boardgame.Position;
+
 public class Program {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        var pos = new Position(3, 5);
+
+        System.out.println(pos);
     }
 
 }
